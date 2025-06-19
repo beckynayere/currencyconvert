3bf6273b4e67b55b27a89ca53cf3de3cbbb4beac
beckynayere <rebecca.nayere@gmail.com>
2025-06-19 18:46:55 +0300

    remove folder

--- frontend/currency-converter/src/services/currencyService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'https://api.exchangerate.host';
-
-export async function getExchangeRate(base, target) {
-  const response = await axios.get(`${API_URL}/latest?base=${base}&symbols=${target}`);
-  return response.data.rates[target];
-}
